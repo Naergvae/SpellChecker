bb5c979c43dfd01e321dce0a4633b0dceb35b0e5
Naergvae <haustovsv@gmail.com>
2019-09-29 11:33:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LevenshteinAutomaton;
+
+namespace SpellCheckerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vocabular = new [] { "мир", "мор", "май"};
+            var word = "мур";
+            var spellChecker = new SpellChecker(vocabular);
+            var corrections1T = spellChecker.GetCorrections1T(word);
+            Console.WriteLine("Corrections with distance = 1");
+            foreach (var correction in corrections1T)
+                Console.WriteLine(correction);
+            var corrections2T = spellChecker.GetCorrections2T(word);
+            Console.WriteLine("Corrections with distance = 2");
+            foreach (var correction in corrections2T)
+                Console.WriteLine(correction);
+        }
+    }
+}
